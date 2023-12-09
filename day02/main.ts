ce50a95eac4f3333cfb0657ed21c62a42197beea
Mark Stickling <mark.stickling@itv.com>
2023-12-09 02:06:37 +0000

    fixup! Solution for Day02 Part1

--- day02/main.ts
+++ day02/main.ts
@@ -20,8 +20,8 @@ export function cleanInput(rawGameInput: string): Game {
   };
 
   facets.forEach((facet) => {
-    let [numberString, color] = facet.split(" ");
-    let number = parseInt(numberString);
+    const [numberString, color] = facet.split(" ");
+    const number = parseInt(numberString);
     cleaned[color as keyof Game] =
       cleaned[color as keyof Game] > number
         ? cleaned[color as keyof Game]
@@ -34,11 +34,6 @@ export function cleanInput(rawGameInput: string): Game {
 if (import.meta.main) {
   const rawInput = await Deno.readTextFile("./input.txt");
   const bag = { red: 12, green: 13, blue: 14 };
-  //   const rawInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  //   Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-  //   Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-  //   Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-  //   Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
   const successfulGames: number[] = [];
   rawInput.split("\n").forEach((line) => {
     const game = cleanInput(line);
