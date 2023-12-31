3c211f9ab743042cdc1f5d0a4943e3fdf57f7b66
Mark Stickling <mark.stickling@itv.com>
2023-12-31 01:32:58 +0000

    Solution Day6 Part1

--- day06/main.ts
+++ day06/main.ts
@@ -1,8 +1,58 @@
-export function add(a: number, b: number): number {
-  return a + b;
+type RaceRecord = { raceDuration: number; distance: number };
+
+export function calcRaceDistance(
+  raceDuration: number,
+  holdDuration: number
+): number {
+  const travelDuration = raceDuration - holdDuration;
+  const distance = travelDuration * holdDuration;
+  return distance;
+}
+
+export function formatRaceRecords(rawInput: string): RaceRecord[] {
+  const races = [];
+  const parts = rawInput.split("\n").map((line) =>
+    line
+      .split(/\s+/)
+      .slice(1)
+      .map((item) => parseInt(item.trim()))
+  );
+  for (let i = 0; i < parts[0].length; i++) {
+    races.push({ raceDuration: parts[0][i], distance: parts[1][i] });
+  }
+  return races;
+}
+
+export function raceWinnings({ raceDuration, distance }: RaceRecord): number {
+  let winCount = 0;
+  for (let i = 0; i < raceDuration; i++) {
+    if (calcRaceDistance(raceDuration, i) > distance) {
+      winCount++;
+    }
+  }
+  return winCount;
+}
+
+export function computeAnswerDay6Part1(rawInput: string) {
+  const races = formatRaceRecords(rawInput);
+  const raceWinCounts: number[] = races.map((race) => raceWinnings(race));
+  const winningsMultiplied = raceWinCounts.reduce(
+    (a: number, b: number) => a * b
+  );
+  return winningsMultiplied;
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const rawInput = await Deno.readTextFile("./input.txt");
+  const answer1 = computeAnswerDay6Part1(rawInput);
+  // const answer2 = computeAnswerDay5Part2(rawInput);
+
+  console.log(
+    `Answer Day5 Part1 – the lowest location-number that corresponds to any of the initial seed-numbers is…: ${answer1}`
+  );
+
+  // console.log(
+  //   `Answer Day5 Part2 – the lowest location-number that corresponds to any of the initial seed-numbers is…: ${answer2}`
+  // );
 }
--- day06/main_test.ts
+++ day06/main_test.ts
@@ -1,6 +1,44 @@
 import { assertEquals } from "https://deno.land/std@0.209.0/assert/mod.ts";
-import { add } from "./main.ts";
+import {
+  calcRaceDistance,
+  formatRaceRecords,
+  raceWinnings,
+  computeAnswerDay6Part1,
+} from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const testRaceDuration = 7;
+
+const testRecords = `Time:      7  15   30
+Distance:  9  40  200`;
+const testPart1Answer = 288;
+
+const testRecordsFormatted = [
+  { raceDuration: 7, distance: 9 },
+  { raceDuration: 15, distance: 40 },
+  { raceDuration: 30, distance: 200 },
+];
+
+Deno.test(function calcRaceDistanceTest() {
+  assertEquals(calcRaceDistance(testRaceDuration, 0), 0);
+  assertEquals(calcRaceDistance(testRaceDuration, 1), 6);
+  assertEquals(calcRaceDistance(testRaceDuration, 2), 10);
+  assertEquals(calcRaceDistance(testRaceDuration, 3), 12);
+  assertEquals(calcRaceDistance(testRaceDuration, 4), 12);
+  assertEquals(calcRaceDistance(testRaceDuration, 5), 10);
+  assertEquals(calcRaceDistance(testRaceDuration, 6), 6);
+  assertEquals(calcRaceDistance(testRaceDuration, 7), 0);
+});
+
+Deno.test(function formatRaceRecordsTest() {
+  assertEquals(formatRaceRecords(testRecords), testRecordsFormatted);
+});
+
+Deno.test(function raceWinningsTest() {
+  assertEquals(raceWinnings({ raceDuration: 7, distance: 9 }), 4);
+  assertEquals(raceWinnings({ raceDuration: 15, distance: 40 }), 8);
+  assertEquals(raceWinnings({ raceDuration: 30, distance: 200 }), 9);
+});
+
+Deno.test(function computeAnswerDay6Part1Test() {
+  assertEquals(computeAnswerDay6Part1(testRecords), testPart1Answer);
 });
